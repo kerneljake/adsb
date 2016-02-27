fe98490817fde2fda0ba501eb0688451b8d14a8e
Jake Angerman <jake.angerman@mongodb.com>
2016-02-26 22:25:36 -0600

    $setOnInsert for topmost timestamp, stdin options more flexible

--- ingest.py
+++ ingest.py
@@ -10,7 +10,7 @@ from pytz import timezone
 import signal
 from pymongo import MongoClient
 
-### CONFIG HERE
+############ CONFIG HERE ############
 # your timezone
 MY_TIMEZONE = 'US/Central'
 # flush interval (in seconds) when reading from stdin
@@ -19,14 +19,14 @@ FLUSH_INTERVAL = 5
 FLUSH_MESSAGE_COUNT = 50
 # database config
 DATABASE = 'adsb'
-COLLECTION = 'tincan'
+COLLECTION = 'testing'
 URI = 'mongodb://localhost:27017/'
-### END CONFIG
+############ END CONFIG #############
 
 def flush_buffer():
 	bulk = collection.initialize_unordered_bulk_op()
 	for key in buffer:
-		update_command = {"$set": { "icao": buffer[key]['icao']}}
+		update_command = {"$set": { "icao": buffer[key]['icao']}, "$setOnInsert": {"ts": buffer[key]['ts']}}
 		if 'callsign' in buffer[key]:
 			update_command["$set"]['callsign'] = buffer[key]['callsign']
 		if 'events' in buffer[key] and len(buffer[key]['events']) > 0:
@@ -35,38 +35,25 @@ def flush_buffer():
 	result = bulk.execute()
 	buffer.clear()
 
-
-def debug_flush_buffer():
-	for key in buffer:
-		print 'bulk.find({"_id": "' + key + '"}.upsert().updateOne({"$set": { "icao": "' + buffer[key]['icao'] + '"',
-		# construct top-level fields
-		if 'callsign' in buffer[key]:
-			print ', "callsign": "' + buffer[key]['callsign'] + '"',
-		print '}',
-		# construct events
-		if 'events' in buffer[key] and len(buffer[key]['events']) > 0:
-			print ', "$push": {"events": {"$each": ',
-			print buffer[key]['events'],
-			print '}}}',
-		print ')'
-	buffer.clear()
-
 def catch_sigint(signal, frame):
 	flush_buffer()
 	sys.exit(0)
 
+# database initialization
 dbclient = MongoClient(URI)
 db = dbclient[DATABASE]
 collection = db[COLLECTION]
 
+# variable initialization
 buffer = {}
 signal.signal(signal.SIGINT, catch_sigint)
 messages_read = 0
 my_tz = timezone(MY_TIMEZONE)
 utc_tz = timezone('UTC')
-from_stdin = True if 1 == len(sys.argv) else False
+from_network = True if 1 == len(sys.argv) else False # note: you can use '-' if you want to uncompress the input
 
-with sys.stdin if from_stdin else open(sys.argv[1], 'rb') as csvfile:
+# main loop
+with sys.stdin if from_network or sys.argv[1] is '-' else open(sys.argv[1], 'rb') as csvfile:
 	reader = csv.reader(csvfile, delimiter=',')
 	start = time.time()
 	for row in reader:
@@ -79,39 +66,50 @@ with sys.stdin if from_stdin else open(sys.argv[1], 'rb') as csvfile:
 		utc_str_hour = local_dt_ms.replace(microsecond = 0, second = 0, minute = 0).astimezone(utc_tz).strftime("%Y%m%d%H") # UTC to the nearest hour
 		_id = utc_str_hour + ":" + icao_hex
 
-		# initialize the topmost dictionary
+		# initialize the dictionary
 		try:
 			buffer[_id]['icao'] = icao_hex
 		except (KeyError):
 			buffer.setdefault(_id, {'icao': icao_hex})
+			# 'ts' is the timestamp of the first message recorded in a given document,
+			# which may precede the timestamp in the first 'event' array element.
+			# The following assignment is not idempotent, but we rectify during initial insert.
+			buffer[_id]['ts'] = utc_dt_second
 
 		# process each message type
 		if "1" == transmission_type:
 			buffer[_id]['callsign'] = callsign.strip()
 		elif "3" == transmission_type:
+			if not (altitude or longitude or latitude):
+				continue
 			try:
 				if utc_dt_second != buffer[_id]['events'][-1]['t']:
 					buffer[_id]['events'].append({})
 			except (KeyError):
 				buffer[_id].setdefault('events', [{}])
 
-			buffer[_id]['events'][-1]['a'] = int(altitude)
+			if altitude:
+				buffer[_id]['events'][-1]['a'] = int(altitude)
 			buffer[_id]['events'][-1]['p'] = [ float(longitude), float(latitude) ]
 			buffer[_id]['events'][-1]['t'] = utc_dt_second
 		elif "4" == transmission_type:
+			if not (speed or bearing or vertical_rate):
+				continue
                         try:
 				if utc_dt_second != buffer[_id]['events'][-1]['t']:
 					buffer[_id]['events'].append({})
                         except (KeyError):
 				buffer[_id].setdefault('events', [{}])
 
-			buffer[_id]['events'][-1]['s'] = int(speed)
-			buffer[_id]['events'][-1]['b'] = int(bearing)
+			if speed:
+				buffer[_id]['events'][-1]['s'] = int(speed)
+			if bearing:
+				buffer[_id]['events'][-1]['b'] = int(bearing)
 			if vertical_rate:
 				buffer[_id]['events'][-1]['v'] = int(vertical_rate)
 			buffer[_id]['events'][-1]['t'] = utc_dt_second
 
-		if from_stdin:
+		if from_network:
 			now = time.time()
 			if now - start >= FLUSH_INTERVAL:
 				flush_buffer()
